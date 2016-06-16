9a64d27765b2d0314be6218f16db503d55e7ac06
mama <antony.denis.7@gmail.com>
2016-06-16 17:53:10 +0200

    ----------------

--- script_vim.sh
+++ script_vim.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 cp -r .vim ~/.vim
 cp .vimrc ~/.vimrc
@@ -7,5 +7,5 @@ chsh -s /bin/zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 cp .zshrc ~/.zshrc
 cp -r .oh-my-zsh/zsh-syntax-highlighting ~/.oh-my-zsh/
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
