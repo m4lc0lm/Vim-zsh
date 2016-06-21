b8f753bcd4cc4b8b692a9479890019b94beabc4a
mama <antony.denis.7@gmail.com>
2016-06-21 16:56:46 +0200

    zsh auto install

--- script_vim.sh
+++ script_vim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 sudo apt-get install vim
 cp -r .vim ~/.vim
 cp .vimrc ~/.vimrc
@@ -6,6 +7,5 @@ sudo apt-get install zsh
 chsh -s /bin/zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 cp .zshrc ~/.zshrc
-cp -r .oh-my-zsh/zsh-syntax-highlighting ~/.oh-my-zsh/
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
