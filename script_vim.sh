09ea4eb2e91f0cf1e4380ec4503b4d265f6d8323
Antony DENIS <adenis@e2r12p15.42.fr>
2016-07-05 16:12:13 +0200

    boom

--- script_vim.sh
+++ script_vim.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install vim
 cp -r .vim ~/.vim
 cp .vimrc ~/.vimrc
-sudo apt-get install zsh
-chsh -s /bin/zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-cp .zshrc ~/.zshrc
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
